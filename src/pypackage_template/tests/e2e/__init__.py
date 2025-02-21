db405ceb7344a2b82680a0d95c5e7d27ecc069e0
Marcus D Sherman <betteridiot@users.noreply.github.com>
2025-02-21 18:46:57 -0500

    Create __init__.py

---
+++ src/pypackage_template/tests/e2e/__init__.py
@@ -0,0 +1,7 @@
+"""
+Initialization module for the end-to-end tests package.
+
+This module provides shared utilities for end-to-end tests.
+"""
+
+# This file is intentionally left empty to mark the directory as a package.
